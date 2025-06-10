64b31041f0e4a6e910442f726ae529e7343e61bc
Saqibaqeel <134763772+Saqibaqeel@users.noreply.github.com>
2025-06-10 20:41:31 +0530

    Update axios.js

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api', 
+    baseURL: 'https://mernstackllmoptimzer-3.onrender.com/api', 
     withCredentials: true, 
 });
 export default axiosInstance
